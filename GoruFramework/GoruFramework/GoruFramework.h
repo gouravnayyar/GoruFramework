089f5d8b08895b0ebec6dcbe081b16bf278d0772
Gourav <gnayyar@vmware.com>
2020-03-25 18:18:38 +0530

    Finished the framework

---
+++ GoruFramework/GoruFramework/GoruFramework.h
@@ -0,0 +1,19 @@
+//
+//  GoruFramework.h
+//  GoruFramework
+//
+//  Created by Gourav Nayyar on 25/03/20.
+//  Copyright © 2020 Gourav Nayyar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GoruFramework.
+FOUNDATION_EXPORT double GoruFrameworkVersionNumber;
+
+//! Project version string for GoruFramework.
+FOUNDATION_EXPORT const unsigned char GoruFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoruFramework/PublicHeader.h>
+
+
---
+++ GoruFramework/GoruFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  GoruFramework
+//
+//  Created by Gourav Nayyar on 25/03/20.
+//  Copyright © 2020 Gourav Nayyar. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+
+    private init() {}
+
+    public static func doSomething() -> String  {
+        return "Some stuff from the framework"
+    }
+}
